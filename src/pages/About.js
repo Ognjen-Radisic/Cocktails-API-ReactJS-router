98eb1072c1ac3d5a1431902900a251d8684159fb
Ognjen Radisic <59142427+Ognjen-Radisic@users.noreply.github.com>
2020-12-07 22:47:28 +0100

    about and error page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,16 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nostrum, ipsa,
+        animi temporibus iure similique laboriosam maiores illum exercitationem
+        a veritatis libero. Repellendus placeat sequi quidem rem, laborum
+        adipisci aperiam, eligendi maiores quam, enim corrupti magnam tempore
+        excepturi quasi molestias ratione.
+      </p>
+    </section>
   )
 }
 
