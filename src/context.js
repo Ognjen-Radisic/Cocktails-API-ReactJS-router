dadaf37ae270ae1bfaf22295f605316f6e12a0ed
Ognjen Radisic <59142427+Ognjen-Radisic@users.noreply.github.com>
2020-12-08 10:22:44 +0100

    fetch cocktails api

--- src/context.js
+++ src/context.js
@@ -9,6 +9,42 @@ const AppProvider = ({ children }) => {
   const [searchTerm, setSearchTerm] = useState('a')
   const [cocktails, setCocktails] = useState([])
 
+  const fetchDrinks = async () => {
+    setLoading(true)
+    try {
+      const response = await fetch(`${url}${searchTerm}`)
+      const data = await response.json()
+      const { drinks } = data
+      if (drinks) {
+        const newCocktails = drinks.map((item) => {
+          const {
+            idDrink,
+            strDrink,
+            strDrinkThumb,
+            strAlcoholic,
+            strGlass,
+          } = item
+          return {
+            id: idDrink,
+            name: strDrink,
+            image: strDrinkThumb,
+            info: strAlcoholic,
+            glass: strGlass,
+          }
+        })
+      } else setCocktails([])
+      setLoading(false)
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchDrinks()
+  }, [searchTerm])
+
+  console.log(cocktails)
   return (
     <AppContext.Provider value={{ loading, cocktails, setSearchTerm }}>
       {children}
